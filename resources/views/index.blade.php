766e3177d5214fc654ab26af381fc274d33d0c11
Darius Matulionis <darius@matulionis.lt>
2016-01-30 21:25:56 +0200

    Fixed #6

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,16 +33,16 @@
     $(function () {
       var url = window.location.search.match(/url=([^&]+)/);
       if (url && url.length > 1) {
-            url = decodeURIComponent(url[1]);
-        } else {
-            url = "{!! $urlToDocs !!}";
-        }
+        url = decodeURIComponent(url[1]);
+      } else {
+        url = "{!! $urlToDocs !!}";
+      }
 
       // Pre load translate...
       if(window.SwaggerTranslator) {
         window.SwaggerTranslator.translate();
-    }
-    window.swaggerUi = new SwaggerUi({
+      }
+      window.swaggerUi = new SwaggerUi({
         url: url,
         dom_id: "swagger-ui-container",
         @if(array_key_exists('validatorUrl', get_defined_vars()))
@@ -51,79 +51,79 @@
         @endif
         supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
         onComplete: function(swaggerApi, swaggerUi){
-            @if(isset($requestHeaders))
-            @foreach($requestHeaders as $requestKey => $requestValue)
-            window.swaggerUi.api.clientAuthorizations.add("{{$requestKey}}", new SwaggerClient.ApiKeyAuthorization("{{$requestKey}}", "{{$requestValue}}", "header"));
-            @endforeach
-            @endif
-            
-            if(typeof initOAuth == "function") {
-                initOAuth({
-                  clientId: "your-client-id",
-                  clientSecret: "your-client-secret-if-required",
-                  realm: "your-realms",
-                  appName: "your-app-name", 
-                  scopeSeparator: ",",
-                  additionalQueryStringParams: {}
-              });
-            }
-
-            if(window.SwaggerTranslator) {
-                window.SwaggerTranslator.translate();
-            }
-
-            $('pre code').each(function(i, e) {
-                hljs.highlightBlock(e)
+          @if(isset($requestHeaders))
+          @foreach($requestHeaders as $requestKey => $requestValue)
+          window.swaggerUi.api.clientAuthorizations.add("{{$requestKey}}", new SwaggerClient.ApiKeyAuthorization("{{$requestKey}}", "{{$requestValue}}", "header"));
+          @endforeach
+          @endif
+          
+          if(typeof initOAuth == "function") {
+            initOAuth({
+              clientId: "your-client-id",
+              clientSecret: "your-client-secret-if-required",
+              realm: "your-realms",
+              appName: "your-app-name", 
+              scopeSeparator: ",",
+              additionalQueryStringParams: {}
             });
+          }
+
+          if(window.SwaggerTranslator) {
+            window.SwaggerTranslator.translate();
+          }
+
+          $('pre code').each(function(i, e) {
+            hljs.highlightBlock(e)
+          });
         },
         
         onFailure: function(data) {
-          log("Unable to Load SwaggerUI");
+          console.log("Unable to Load SwaggerUI");
         },
         docExpansion: "none",
         jsonEditor: false,
         apisSorter: "alpha",
         defaultModelRendering: 'schema',
         showRequestHeaders: false
-    });
+      });
 
-        function addApiKeyAuthorization(){
-            var key = encodeURIComponent($('#input_apiKey')[0].value);
-            if(key && key.trim() != "") {
-                var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization("api_key", key, "query");
-                window.swaggerUi.api.clientAuthorizations.add("api_key", apiKeyAuth);
-            }
+      function addApiKeyAuthorization(){
+        var key = encodeURIComponent($('#input_apiKey')[0].value);
+        if(key && key.trim() != "") {
+          var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization("{{$apiKeyVar}}", key, "{{$apiKeyInject}}");
+          window.swaggerUi.api.clientAuthorizations.add("{{$apiKeyVar}}", apiKeyAuth);
         }
+      }
 
-        $('#input_apiKey').change(function() {
-            addApiKeyAuthorization();
-        });
-
-        // if you have an apiKey you would like to pre-populate on the page for demonstration purposes
-        // just put it in the .env file, API_AUTH_TOKEN variable
-        @if($apiKey)
-        $('#input_apiKey').val("{{$apiKey}}");
+      $('#input_apiKey').change(function() {
         addApiKeyAuthorization();
-        @endif
+      });
 
-        window.swaggerUi.load();
+      // if you have an apiKey you would like to pre-populate on the page for demonstration purposes
+      // just put it in the .env file, API_AUTH_TOKEN variable
+      @if($apiKey)
+      $('#input_apiKey').val("{{$apiKey}}");
+      addApiKeyAuthorization();
+      @endif
+
+      window.swaggerUi.load();
     });
 </script>
 </head>
 
 <body class="swagger-section">
-    <div id='header'>
-      <div class="swagger-ui-wrap">
-        <a id="logo" href="http://swagger.io">swagger</a>
-        <form id='api_selector'>
-          <div class='input'><input placeholder="http://example.com/api" id="input_baseUrl" name="baseUrl" type="text"/></div>
-          <div class='input'><input placeholder="api_key" id="input_apiKey" name="apiKey" type="text"/></div>
-          <div class='input'><a id="explore" href="#" data-sw-translate>Explore</a></div>
+  <div id='header'>
+    <div class="swagger-ui-wrap">
+      <a id="logo" href="http://swagger.io">swagger</a>
+      <form id='api_selector'>
+        <div class='input'><input placeholder="http://example.com/api" id="input_baseUrl" name="baseUrl" type="text"/></div>
+        <div class='input'><input placeholder="api_key" id="input_apiKey" name="apiKey" type="text"/></div>
+        <div class='input'><a id="explore" href="#" data-sw-translate>Explore</a></div>
       </form>
+    </div>
   </div>
-</div>
 
-<div id="message-bar" class="swagger-ui-wrap" data-sw-translate>&nbsp;</div>
-<div id="swagger-ui-container" class="swagger-ui-wrap"></div>
+  <div id="message-bar" class="swagger-ui-wrap" data-sw-translate>&nbsp;</div>
+  <div id="swagger-ui-container" class="swagger-ui-wrap"></div>
 </body>
 </html>
