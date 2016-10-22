3520dd91463ecdf8a29471149cf22260369a5b5b
vipus <vipaware@gmail.com>
2016-10-22 18:53:15 +0300

    Update to swagger-ui 2.2.2
    - Didn't update it to latest version (currently v2.2.6) since it has models that just feel broken (no tooltips, less accurate output, etc). So I updated it to v2.2.2 instead, same version used on petstore.
    - I'm also using `apisSorter: "alpha"` which is not used in index.html v2.2.x. I decided to leave it intact (just like on petstore).
    - Also removed four config parameters due to interface has changed and that functionality is also removed from index.html.
    - Using swagger-ui.min.js instead of not minified swagger-ui.js.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,6 +17,8 @@
           type='text/css'/>
     <link href='{{config('swagger-lume.paths.assets_public')}}/css/print.css' media='print' rel='stylesheet'
           type='text/css'/>
+    <script src='{{config('swagger-lume.paths.assets_public')}}/lib/object-assign-pollyfill.js'
+            type='text/javascript'></script>
     <script src='{{config('swagger-lume.paths.assets_public')}}/lib/jquery-1.8.0.min.js'
             type='text/javascript'></script>
     <script src='{{config('swagger-lume.paths.assets_public')}}/lib/jquery.slideto.min.js'
@@ -25,12 +27,14 @@
             type='text/javascript'></script>
     <script src='{{config('swagger-lume.paths.assets_public')}}/lib/jquery.ba-bbq.min.js'
             type='text/javascript'></script>
-    <script src='{{config('swagger-lume.paths.assets_public')}}/lib/handlebars-2.0.0.js'
+    <script src='{{config('swagger-lume.paths.assets_public')}}/lib/handlebars-4.0.5.js'
             type='text/javascript'></script>
-    <script src='{{config('swagger-lume.paths.assets_public')}}/lib/underscore-min.js' type='text/javascript'></script>
+    <script src='{{config('swagger-lume.paths.assets_public')}}/lib/lodash.min.js' type='text/javascript'></script>
     <script src='{{config('swagger-lume.paths.assets_public')}}/lib/backbone-min.js' type='text/javascript'></script>
-    <script src='{{config('swagger-lume.paths.assets_public')}}/swagger-ui.js' type='text/javascript'></script>
-    <script src='{{config('swagger-lume.paths.assets_public')}}/lib/highlight.7.3.pack.js'
+    <script src='{{config('swagger-lume.paths.assets_public')}}/swagger-ui.min.js' type='text/javascript'></script>
+    <script src='{{config('swagger-lume.paths.assets_public')}}/lib/highlight.9.1.0.pack.js'
+            type='text/javascript'></script>
+    <script src='{{config('swagger-lume.paths.assets_public')}}/lib/highlight.9.1.0.pack_extended.js'
             type='text/javascript'></script>
     <script src='{{config('swagger-lume.paths.assets_public')}}/lib/jsoneditor.min.js' type='text/javascript'></script>
     <script src='{{config('swagger-lume.paths.assets_public')}}/lib/marked.js' type='text/javascript'></script>
@@ -49,6 +53,10 @@
             } else {
                 url = "{!! $urlToDocs !!}";
             }
+
+            hljs.configure({
+                highlightSizeThreshold: 5000
+            });
 
             // Pre load translate...
             if (window.SwaggerTranslator) {
@@ -78,7 +86,7 @@
                             clientSecret: "your-client-secret-if-required",
                             realm: "your-realms",
                             appName: "your-app-name",
-                            scopeSeparator: ",",
+                            scopeSeparator: " ",
                             additionalQueryStringParams: {}
                         });
                     }
@@ -86,10 +94,6 @@
                     if (window.SwaggerTranslator) {
                         window.SwaggerTranslator.translate();
                     }
-
-                    $('pre code').each(function (i, e) {
-                        hljs.highlightBlock(e)
-                    });
                 },
 
                 onFailure: function (data) {
@@ -104,29 +108,6 @@
 
             window.swaggerUi.load();
 
-            function addApiKeyAuthorization() {
-                var key = encodeURIComponent($('#input_apiKey')[0].value);
-                if (key && key.trim() != "") {
-                    var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization(
-                        "{{$apiKeyVar}}",
-                        "{{$apiKeyPrefix}}" + key,
-                        "{{$apiKeyInject}}"
-                    );
-                    window.swaggerUi.api.clientAuthorizations.add("{{$apiKeyVar}}", apiKeyAuth);
-                }
-            }
-
-            $('#input_apiKey').change(function () {
-                addApiKeyAuthorization();
-            });
-
-            // if you have an apiKey you would like to pre-populate on the page for demonstration purposes
-            // just put it in the .env file, API_AUTH_TOKEN variable
-            @if($apiKey)
-            $('#input_apiKey').val("{{$apiKey}}");
-            addApiKeyAuthorization();
-            @endif
-
         });
     </script>
 </head>
@@ -134,12 +115,16 @@
 <body class="swagger-section">
 <div id='header'>
     <div class="swagger-ui-wrap">
-        <a id="logo" href="http://swagger.io">swagger</a>
+        <a id="logo" href="http://swagger.io">
+            <img class="logo__img" alt="swagger" height="30" width="30"
+                 src="{{config('swagger-lume.paths.assets_public')}}/images/logo_small.png" />
+            <span class="logo__title">swagger</span>
+        </a>
         <form id='api_selector'>
             <div class='input'><input placeholder="http://example.com/api" id="input_baseUrl" name="baseUrl"
                                       type="text"/></div>
-            <div class='input'><input placeholder="api_key" id="input_apiKey" name="apiKey" type="text"/></div>
-            <div class='input'><a id="explore" href="#" data-sw-translate>Explore</a></div>
+            <div id='auth_container'></div>
+            <div class='input'><a id="explore" class="header__btn" href="#" data-sw-translate>Explore</a></div>
         </form>
     </div>
 </div>
