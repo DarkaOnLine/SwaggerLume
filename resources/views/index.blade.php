ba30de4e2915e9da66889c78fb8c1d95fa148bbd
Ajay Patel <ajaypatel@atyantik.com>
2016-12-20 18:55:46 +0530

    Issue fixed for combox.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,86 +42,86 @@
     <!-- <script src='lang/en.js' type='text/javascript'></script> -->
 
     <script type="text/javascript">
-        $(function () {
-            var url = window.location.search.match(/url=([^&]+)/);
-            if (url && url.length > 1) {
-                url = decodeURIComponent(url[1]);
-            } else {
-                url = "{!! $urlToDocs !!}";
-            }
+      $(function () {
+        var url = window.location.search.match(/url=([^&]+)/);
+        if (url && url.length > 1) {
+          url = decodeURIComponent(url[1]);
+        } else {
+          url = "{!! $urlToDocs !!}";
+        }
 
-            // Pre load translate...
+        // Pre load translate...
+        if (window.SwaggerTranslator) {
+          window.SwaggerTranslator.translate();
+        }
+        window.swaggerUi = new SwaggerUi({
+          url: url,
+          dom_id: "swagger-ui-container",
+            @if(array_key_exists('validatorUrl', get_defined_vars()))
+          // This differentiates between a null value and an undefined variable
+          validatorUrl: {!! isset($validatorUrl) ? '"' . $validatorUrl . '"' : 'null' !!},
+            @endif
+            supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
+          onComplete: function (swaggerApi, swaggerUi) {
+            @if(isset($requestHeaders))
+            @foreach($requestHeaders as $requestKey => $requestValue)
+            window.swaggerUi.api.clientAuthorizations.add(
+              "{{$requestKey}}",
+              new SwaggerClient.ApiKeyAuthorization("{{$requestKey}}", "{{$requestValue}}", "header")
+            );
+              @endforeach
+                      @endif
+
+              if (typeof initOAuth == "function") {
+                initOAuth({
+                  clientId: "your-client-id",
+                  clientSecret: "your-client-secret-if-required",
+                  realm: "your-realms",
+                  appName: "your-app-name",
+                  scopeSeparator: ",",
+                  additionalQueryStringParams: {}
+                });
+              }
+
             if (window.SwaggerTranslator) {
-                window.SwaggerTranslator.translate();
+              window.SwaggerTranslator.translate();
             }
-            window.swaggerUi = new SwaggerUi({
-                url: url,
-                dom_id: "swagger-ui-container",
-                @if(array_key_exists('validatorUrl', get_defined_vars()))
-                // This differentiates between a null value and an undefined variable
-                validatorUrl: {!! isset($validatorUrl) ? '"' . $validatorUrl . '"' : 'null' !!},
-                @endif
-                supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
-                onComplete: function (swaggerApi, swaggerUi) {
-                    @if(isset($requestHeaders))
-                    @foreach($requestHeaders as $requestKey => $requestValue)
-                    window.swaggerUi.api.clientAuthorizations.add(
-                        "{{$requestKey}}",
-                        new SwaggerClient.ApiKeyAuthorization("{{$requestKey}}", "{{$requestValue}}", "header")
-                    );
-                    @endforeach
-                            @endif
 
-                    if (typeof initOAuth == "function") {
-                        initOAuth({
-                            clientId: "your-client-id",
-                            clientSecret: "your-client-secret-if-required",
-                            realm: "your-realms",
-                            appName: "your-app-name",
-                            scopeSeparator: ",",
-                            additionalQueryStringParams: {}
-                        });
-                    }
-
-                    if (window.SwaggerTranslator) {
-                        window.SwaggerTranslator.translate();
-                    }
-
-                    $('pre code').each(function (i, e) {
-                        hljs.highlightBlock(e)
-                    });
-                },
-
-                onFailure: function (data) {
-                    console.log("Unable to Load SwaggerUI");
-                },
-                docExpansion: "none",
-                jsonEditor: false,
-                apisSorter: "alpha",
-                defaultModelRendering: 'schema',
-                showRequestHeaders: false
+            $('pre code').each(function (i, e) {
+              hljs.highlightBlock(e)
             });
+          },
 
-            window.swaggerUi.load();
-
-            $('#input_AppUrl').change(function(){
-                var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization(
-                        "AppUrl",
-                        $(this).val(),
-                        "header"
-                    );
-                window.swaggerUi.api.clientAuthorizations.add("AppUrl", apiKeyAuth);
-            });
-            $('#input_JwtToken').change(function(){
-                 var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization(
-                        "WJWT-Authorization",
-                        $(this).val(),
-                        "header"
-                    );
-                window.swaggerUi.api.clientAuthorizations.add("WJWT-Authorization", apiKeyAuth);
-            });
-
+          onFailure: function (data) {
+            console.log("Unable to Load SwaggerUI");
+          },
+          docExpansion: "none",
+          jsonEditor: false,
+          apisSorter: "alpha",
+          defaultModelRendering: 'schema',
+          showRequestHeaders: false
         });
+
+        window.swaggerUi.load();
+
+        $('#input_AppUrl').change(function(){
+          var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization(
+            "AppUrl",
+            $(this).val(),
+            "header"
+          );
+          window.swaggerUi.api.clientAuthorizations.add("AppUrl", apiKeyAuth);
+        });
+        $('#input_JwtToken').change(function(){
+          var apiKeyAuth = new SwaggerClient.ApiKeyAuthorization(
+            "WJWT-Authorization",
+            $(this).val(),
+            "header"
+          );
+          window.swaggerUi.api.clientAuthorizations.add("WJWT-Authorization", apiKeyAuth);
+        });
+
+      });
     </script>
 </head>
 
@@ -139,15 +139,16 @@
 
 <div id="message-bar" class="swagger-ui-wrap" data-sw-translate>&nbsp;</div>
 <div class="swagger-ui-wrap">
-            <div class='input'>
-                <select placeholder="AppUrl" id="input_AppUrl" name="AppUrl" type="text">
-                <option value="http://www.waverfid.dev">http://www.waverfid.dev</option>
-                @foreach ($clients as $client)
-                    <option value="{{'http://'.$client->domain}}">{{ $client->domain }}</option>
-                @endforeach
-                </select>
-            </div>
-            <div class='input'><input placeholder="JwtToken" id="input_JwtToken" name="JwtToken" type="text"/></div>
+    <div class='input'>
+        <select placeholder="AppUrl" id="input_AppUrl" name="AppUrl" type="text">
+            <option value="">Select Client</option>
+            <option value="http://www.waverfid.dev">http://www.waverfid.dev</option>
+            @foreach ($clients as $client)
+                <option value="{{'http://'.$client->domain}}">{{ $client->domain }}</option>
+            @endforeach
+        </select>
+    </div>
+    <div class='input'><input placeholder="JwtToken" id="input_JwtToken" name="JwtToken" type="text"/></div>
 </div>
 <div id="swagger-ui-container" class="swagger-ui-wrap"></div>
 </body>
