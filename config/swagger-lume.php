faa916139fcd88d2456f68665123bb5467cba766
ADReece <103013045+ADReece@users.noreply.github.com>
2023-05-05 12:33:27 +0100

    Updated swagger-lume.php config
    Updated config file to conform to styling.

--- config/swagger-lume.php
+++ config/swagger-lume.php
@@ -207,6 +207,5 @@ return [
     | Force assets to be loaded over HTTPS (Solves mixed content errors when application is behind a load balancer.
     |--------------------------------------------------------------------------
      */
-    
-    'force_https' => false
+    'force_https' => false,
 ];
