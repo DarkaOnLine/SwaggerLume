f398ce552be83d031e4bcb14b24c38c9a7a979b7
R Mathieson <r.mathieson@oneandall.co.uk>
2023-05-09 12:53:51 +0100

    Final cleanup

--- config/swagger-lume.php
+++ config/swagger-lume.php
@@ -204,7 +204,7 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Force assets to be loaded over HTTPS (Solves mixed content errors when application is behind a load balancer.
+    | Force assets to be loaded over HTTPS (Solves mixed content errors when application is behind a load balancer.)
     |--------------------------------------------------------------------------
      */
     'force_https' => env('SWAGGER_LUME_FORCE_HTTPS', false),
