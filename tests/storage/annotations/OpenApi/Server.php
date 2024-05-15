db39bddfc7755ea5b372685a0b5cff861653bda9
Darius Matulionis <darius.matulionis@supermetrics.com>
2024-05-15 08:31:38 +0300

    separate tests anotations to files for swagger-php:4 support

---
+++ tests/storage/annotations/OpenApi/Server.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\storage\annotations\OpenApi;
+
+
+/**
+ *  @OA\Server(
+ *      url=SWAGGER_LUME_CONST_HOST,
+ *      description="SwaggerLume OpenApi dynamic host server"
+ *  )
+ *
+ *  @OA\Server(
+ *      url="https://projects.dev/api/v1",
+ *      description="SwaggerLume OpenApi Server"
+ * )
+ */
+class Server
+{
+}
