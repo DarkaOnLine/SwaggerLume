9e7fb1bc21ca7a1536f6866678b91188bad1725a
Mark Vaughn <IfTrueElseFalse@gmail.com>
2024-05-14 23:09:37 -0700

    Support Lumen 11 (#140)
    * Updating CI
    
    * Update composer.json
    
    * zircot
    
    * Update test-config.yml
    
    * Update test-config.yml
    
    * Latest
    
    * Update test-config.yml
    
    * Update test-config.yml
    
    * Update test-config.yml
    
    * Update composer.json
    
    * Update test-config.yml
    
    * Update composer.json
    
    * Update composer.json
    
    * Update composer.json
    
    * Update test-config.yml
    
    * Update test-config.yml
    
    * Update test-config.yml
    
    * Update test-config.yml
    
    * Update test-config.yml
    
    * Update test-config.yml
    
    * separate tests anotations to files for swagger-php:4 support
    
    ---------
    
    Co-authored-by: Darius Matulionis <darius.matulionis@supermetrics.com>

---
+++ tests/storage/annotations/OpenApi/Info.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\storage\annotations\OpenApi;
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="SwaggerLume OpenApi",
+ *      description="SwaggerLume OpenApi description",
+ *      @OA\Contact(
+ *          email="darius@matulionis.lt"
+ *      ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     ),
+ *     @OA\PathItem(
+ *       path="/"
+ *     )
+ * )
+ */
+class Info
+{
+}
